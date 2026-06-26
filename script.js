4a1ea0ad4d1618a98ed8da321ab4d09b9158480c
4he4opka <logic2280@gmail.com>
2026-06-26 17:14:44 +0200

    Added JS form validation, going to style.css

--- script.js
+++ script.js
@@ -6,7 +6,39 @@ const workshop = document.getElementById("workshop");
 const terms = document.getElementById("terms");
 const message = document.getElementById("message");
 
-form.addEventListener("submit", function(event) {
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
 
-    
-})
+    if (fullName.value.trim() === "") {
+        message.textContent = "Full name is required.";
+        return;
+    }
+
+    if (!email.value.includes("@")) {
+        message.textContent = "Email is invalid.";
+        return;
+    }
+
+    if (Number(age.value) <= 0) {
+        message.textContent = "Age is not valid.";
+        return;
+    }
+
+    if (Number(age.value) < 18) {
+        message.textContent = "Age must be at least 18.";
+        return;
+    }
+
+    if (workshop.value === "") {
+        message.textContent = "Please select a workshop.";
+        return;
+    }
+
+    if (!terms.checked) {
+        message.textContent = "You must accept the terms.";
+        return;
+    }
+
+    message.textContent =
+        fullName.value + " has been signed up for the workshop.";
+});
