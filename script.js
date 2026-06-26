1037a50922a77091d9ee41693cc99d864bd7b180
4he4opka <logic2280@gmail.com>
2026-06-26 16:54:43 +0200

    Finish HTML registration form and started doing js

---
+++ script.js
@@ -0,0 +1,12 @@
+const form = document.getElementById("registrationForm");
+const fullName = document.getElementById("fullName");
+const email = document.getElementById("email");
+const age = document.getElementById("age");
+const workshop = document.getElementById("workshop");
+const terms = document.getElementById("terms");
+const message = document.getElementById("message");
+
+form.addEventListener("submit", function(event) {
+
+    
+})
