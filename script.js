a8fbd09482688fbc95eee2dc4a8991068549f006
4he4opka <logic2280@gmail.com>
2026-06-26 18:00:53 +0200

    Finalize project and add README

--- script.js
+++ script.js
@@ -10,35 +10,44 @@ form.addEventListener("submit", function (event) {
     event.preventDefault();
 
     if (fullName.value.trim() === "") {
+        message.className = "error";
         message.textContent = "Full name is required.";
         return;
     }
 
     if (!email.value.includes("@")) {
+        message.className = "error";
         message.textContent = "Email is invalid.";
         return;
     }
 
     if (Number(age.value) <= 0) {
+        message.className = "error";
         message.textContent = "Age is not valid.";
         return;
     }
 
     if (Number(age.value) < 18) {
+        message.className = "error";
         message.textContent = "Age must be at least 18.";
         return;
     }
 
     if (workshop.value === "") {
+        message.className = "error";
         message.textContent = "Please select a workshop.";
         return;
     }
 
     if (!terms.checked) {
+        message.className = "error";
         message.textContent = "You must accept the terms.";
         return;
     }
-
+    message.className = "success";
+    
     message.textContent =
-        fullName.value + " has been signed up for the workshop.";
+        fullName.value + " has been signed up for the workshop.\n" +
+        "Track: " + workshop.value + "\n" +
+        "Age: " + age.value;
 });
